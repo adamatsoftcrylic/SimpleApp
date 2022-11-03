d0178f0933f6b228abce5544716b35f57fbcb691
adamatsoftcrylic <adam.i@softcrylic.com>
2022-11-03 14:31:32 -0500

    Initial Commit

---
+++ SimpleApp/SimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppApp.swift
+//  SimpleApp
+//
+//  Created by Adam Iverson on 11/3/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
